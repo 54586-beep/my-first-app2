d57329553a9c46f23f7a47048851f8c1b673ab3a
402-34-tunyathip <54586@yupparaj.ac.th>
2026-08-19 15:19:24 +0700

    Update vocap_game.py

--- vocap_game.py
+++ vocap_game.py
@@ -67,7 +67,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 2:
+    if score == 4:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
